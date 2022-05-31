203c4d36773563a3058f251c083451802d6f4089
Jaba Makharashvili <m017735i@student.staffs.ac.uk>
2022-05-31 12:33:43 +0100

    SpawnScript

--- New Unity Project/Assets/SpawnScript.cs
+++ New Unity Project/Assets/SpawnScript.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class SpawnScript : MonoBehaviour
 {
-    [SerializeField] GameObject m_objToSpawn;
+    [SerializeField] List<GameObject> m_enemies;
+    GameObject m_WaveManager;
 
     List<Vector3> m_spawnPoints;
+    List<Vector3> m_spawnedObjects;
+
+    float m_spawnTimer;
+    [SerializeField] float m_spawnRate;
 
     // Start is called before the first frame update
     void Start()
     {
+        m_spawnPoints = new List<Vector3>();
         for (int i = 0; i < transform.childCount; i++)
         {
-
+            m_spawnPoints.Add(transform.GetChild(i).position);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        m_spawnTimer += Time.deltaTime;
         
+        if (m_spawnTimer >= m_spawnRate)
+        {
+            int index = Random.Range(0, m_spawnPoints.Count);
+            int enemyIndex = Random.Range(0, m_enemies.Count);
+
+            Instantiate(m_enemies[enemyIndex], m_spawnPoints[index], Quaternion.identity);
+            m_spawnTimer = 0.0f;
+        }
     }
 }
